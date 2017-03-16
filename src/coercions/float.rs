36240752836a147d18f46b9260f23322fbc64f74
Peter Wagenet <peter.wagenet@gmail.com>
2017-03-16 12:42:28 -0700

    Convert fixnum to float

--- src/coercions/float.rs
+++ src/coercions/float.rs
@@ -1,11 +1,11 @@
-use sys::{self, VALUE, T_FLOAT};
+use sys::{self, VALUE, T_FLOAT, T_FIXNUM};
 use std::ffi::CString;
 
 use super::{UncheckedValue, CheckResult, CheckedValue, ToRust, ToRuby};
 
 impl UncheckedValue<f64> for VALUE {
     fn to_checked(self) -> CheckResult<f64> {
-        if unsafe { sys::RB_TYPE_P(self, T_FLOAT) } {
+        if unsafe { sys::RB_TYPE_P(self, T_FLOAT) || sys::RB_TYPE_P(self, T_FIXNUM) } {
             Ok(unsafe { CheckedValue::new(self) })
         } else {
             let val = unsafe { CheckedValue::<String>::new(sys::rb_inspect(self)) };
@@ -16,7 +16,11 @@ impl UncheckedValue<f64> for VALUE {
 
 impl ToRust<f64> for CheckedValue<f64> {
     fn to_rust(self) -> f64 {
-        unsafe { sys::FLOAT2F64(self.inner) }
+        if unsafe { sys::RB_TYPE_P(self.inner, T_FLOAT) } {
+            unsafe { sys::FLOAT2F64(self.inner) }
+        } else {
+            unsafe { sys::NUM2I64(self.inner) as f64 }
+        }
     }
 }
 
